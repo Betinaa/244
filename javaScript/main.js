63c4e7c812a8c388b65d8ac3c2423407780e99be
Betinaa <justoritter@gmail.com>
2023-04-12 07:53:57 -0300

    atividade 2

---
+++ javaScript/main.js
@@ -0,0 +1,34 @@
+// console.log("pera, que?")
+
+// const nome = 'bezinha'
+// const sobrenome = 'justo'
+// const idade = 17
+// const boolean = true
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+// console.log(typeof sobrenome)
+// console.log(typeof boolean)
+
+// const nomeDoUsuario = prompt('Digite seu nome')
+// const idadeDoUsuario = prompt('Digite sua iade')
+
+// console.log("Meu nome é"+ nome+ "e tenho" +idade+ "anos")
+// console.log("Meu nome é" ,nome, "e tenho" ,idade, "anos")
+// console.log(`Meu nome é ${nome} e tenho ${idade} anos`) - favorita da sora
+
+// crie duas variaveis e peça para o usuario informar dois numeros. Faça a soma desses dois numeros e imprima no console
+
+const var1 = parseInt(prompt('Digite um numero'))
+const var2  = parseInt(prompt('Digite outro numero'))
+
+const soma = (var1 + var2)
+
+alert(` seu resultado é ${soma}`)
+
+
+
+
+
+
+
